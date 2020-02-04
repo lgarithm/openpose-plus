188600b27a682d19140ae9c120506a88e5495010
lg <lg4869@outlook.com>
2020-02-04 22:39:00 +0000

    imread_resize

--- src/example-main.cpp
+++ src/example-main.cpp
@@ -6,6 +6,8 @@
 #include <string>
 
 #include <stdtracer>
+#include <ttl/contrib/opencv>
+#include <ttl/cuda_tensor>
 #include <ttl/nn/layers>
 #include <ttl/range>
 
@@ -44,15 +46,12 @@ int main(int argc, char *argv[])
     auto paf_runner =
         create_paf_processor(32, 48, openpose.h, openpose.w, 19, 19, 13);
 
-    auto x = ttl::tensor<float, 4>(1, openpose.h, openpose.w, 3);
+    auto x = ttl::tensor<float, 3>(openpose.h, openpose.w, 3);
 
     // TODO: input images
-    auto input = ttl::tensor<uint8_t, 4>(x.shape());
-    cv::Mat resized_image(cv::Size(openpose.w, openpose.h), CV_8UC(3),
-                          input.data());
+    auto input = ttl::tensor<uint8_t, 3>(x.shape());
     {
-        auto img = cv::imread(filename);
-        cv::resize(img, resized_image, resized_image.size(), 0, 0);
+        ttl::cv::imread_resize(filename.c_str(), ttl::ref(input));
         std::transform(input.data(), input.data_end(), x.data(),
                        [](uint8_t p) { return p / 255.0; });
     }
@@ -60,8 +59,9 @@ int main(int argc, char *argv[])
     int repeats = 5;
     for (auto i : ttl::range(repeats)) {
         printf("inference %d\n", i);
-        auto [l_conf, l_paf] = openpose(ttl::ref(x));
-
+        ttl::tensor_ref<float, 4> x_batch(x.data(), 1, openpose.h, openpose.w,
+                                          3);
+        auto [l_conf, l_paf] = openpose(x_batch);
         // TODO: run paf process
         auto conf = ttl::nn::ops::apply<ttl::tensor<float, 4>>(
             ttl::nn::ops::to_channels_first(), *l_conf);
@@ -69,11 +69,14 @@ int main(int argc, char *argv[])
             ttl::nn::ops::to_channels_first(), *l_paf);
 
         auto human = (*paf_runner)(conf.data(), paf.data(), false);
-        for (auto h : human) {
-            h.print();
-            draw_human(resized_image, h);
+        {
+            auto resized_image = ttl::cv::as_cv_mat(input);
+            for (auto h : human) {
+                h.print();
+                draw_human(resized_image, h);
+            }
+            cv::imwrite("a.png", resized_image);
         }
-        cv::imwrite("a.png", resized_image);
     }
     return 0;
 }
